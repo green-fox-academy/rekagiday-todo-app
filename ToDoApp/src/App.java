9a742a42791dd846ad9c8c25cdd00d77984e7a25
Reka Giday <giday.r@gmail.com>
2017-04-06 12:10:54 +0200

    add print usage if args=0

--- ToDoApp/src/App.java
+++ ToDoApp/src/App.java
@@ -1,8 +1,22 @@
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.Files;
 import java.util.Arrays;
+import java.util.List;
 
 public class App {
 
   public static void main(String[] args) {
-    System.out.println(Arrays.toString(args));
+    Path filePath = Paths.get("usage.txt");
+    List<String> lines = null;
+    try {
+      lines = Files.readAllLines(filePath);
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+    for (int i = 0; i < lines.size(); i++) {
+      System.out.println(lines.get(i));
+    }
   }
 }
